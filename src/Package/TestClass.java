8ee4f8de6a98bcddbcaa6d9edeadaed772847e1e
win10 <win10@DESKTOP-89GHF6S>
2024-02-12 22:55:04 +0900

    This is Commit Message

---
+++ src/Package/TestClass.java
@@ -0,0 +1,12 @@
+package Package;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello! This is Github Practicing!");
+		//Github Practice!
+
+	}
+
+}
