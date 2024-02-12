5f16fd3224933f36dfb3e8d4ecba1340d9bf9d48
win10 <win10@DESKTOP-89GHF6S>
2024-02-12 23:50:14 +0900

    test

--- src/Package/TestClass.java
+++ src/Package/TestClass.java
@@ -5,7 +5,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello! This is Github Practicing!");
-		//Github Practice!
+		//Github Practice!!
 
 	}
 
